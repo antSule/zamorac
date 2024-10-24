0a91dc9ed1fd5e4c9fe65291a6360597b16bf03d
Antonio Šulenta <167515119+antSule@users.noreply.github.com>
2024-10-24 18:41:30 +0200

    Osnovni autentifikacijski sustav
    Napravljena registracija i spremanje novih usera u bazu podataka pomoću PostgreSQL i testirano koristeći Postman.

---
+++ ticketmestar/src/main/java/com/zamorac/ticketmestar/registration/RegistrationService.java
@@ -0,0 +1,28 @@
+package com.zamorac.ticketmestar.registration;
+
+import com.zamorac.ticketmestar.appuser.AppUser;
+import com.zamorac.ticketmestar.appuser.AppUserRole;
+import com.zamorac.ticketmestar.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException("E-mail not valid.");
+        }
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
+    }
+}
---
+++ ticketmestar/src/test/java/com/zamorac/ticketmestar/TicketmestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zamorac.ticketmestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
