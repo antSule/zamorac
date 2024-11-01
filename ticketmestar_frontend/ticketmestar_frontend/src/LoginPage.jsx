0d0c6e40f88fe3a462c4c8b19cd7aa056853117c
AnteSule <antesulenta1@gmail.com>
2024-11-01 19:16:25 +0100

    Dodan frontend i login page

---
+++ ticketmestar_frontend/ticketmestar_frontend/src/LoginPage.jsx
@@ -0,0 +1,53 @@
+import { Avatar, Box, Button, Container, FormControlLabel, Grid, Link, Paper, TextField, Typography } from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { CheckBox } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+
+const LoginPage = () => {
+    const handleSubmit = () => console.log("Log in!")
+    return (
+        <Container maxWidth="xs">
+            <Paper elevation={10} sx={{ marginTop: 8, padding: 2}}>
+                <Avatar sx={{
+                    mx: "auto",
+                    bgcolor: "secondary.main",
+                    textAlign: "center",
+                    mb: 1
+                }}>
+                    <LockOutlinedIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5" sx={{textAlign:"center"}}>
+                    Sign In
+                </Typography>
+                <Box
+                    component='form'
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{ mt: 1 }}
+                >
+                    <TextField
+                        placeholder="Email"
+                        fullWidth
+                        required
+                        autoFocus
+                        sx={{mb: 2}}
+                    />
+                    <TextField
+                        placeholder="Password"
+                        fullWidth
+                        required
+                        type="password"
+                    />
+                    <FormControlLabel control={<CheckBox value="remember" color="primary"/>}
+                                      label="Remember me"
+                    />
+                    <Button type="submit" variant="contained" fullWidth sx={{mt: 1}}>
+                        Sign In
+                    </Button>
+                </Box>
+            </Paper>
+        </Container>
+    )
+};
+
+export default LoginPage;
