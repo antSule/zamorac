d33c7f93ddcc1cd9bbe687d6002ecbbce09b8a24
LM-99 <lm54840@fer.hr>
2024-11-07 23:43:14 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import 'swiper/css';
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import 'swiper/css/effect-coverflow';
+import HomePage from './home/HomePage';
+
+
+function App() {
+  return (
+    <div>
+    <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/home/HomePage.jsx
@@ -0,0 +1,109 @@
+import { Link as RouterLink } from "react-router-dom";
+import './home.css';
+import './header.css';
+import {EffectCoverflow,Pagination,Navigation} from 'swiper/modules';
+import {Swiper,SwiperSlide,useSwiper} from 'swiper/react'
+import './slajder.css'
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import slide_image_1 from '../slike/img_1.jpg';
+import slide_image_2 from '../slike/img_2.jpg';
+import slide_image_3 from '../slike/img_3.jpg';
+import slide_image_4 from '../slike/img_4.jpg';
+import slide_image_5 from '../slike/img_5.jpg';
+import slide_image_6 from '../slike/img_6.jpg';
+import slide_image_7 from '../slike/img_7.jpg';
+import React from 'react';
+
+const HomePage = () =>{
+    const handleButtonClick = () =>console.log("Klik");
+    const slideImages = [
+        slide_image_1,
+        slide_image_2,
+        slide_image_3,
+        slide_image_4,
+        slide_image_5,
+        slide_image_6,
+        slide_image_7,
+      ];
+    return(
+    <>
+       <section className=" h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+            <img src="fakelogo.png" alt="logo"width = {100} />
+             
+             <div className="flexCenter h-menu">
+                <a href="">Favourites</a>
+                <a href="">Filter</a>
+                <button className="button">
+                <a href="">Connect spotify</a>
+                </button>
+             </div>
+        </div>
+       </section>
+       
+       <div className="searchBarWrapper">
+         <input className="searchBar" type="text" placeholder="Search..." />
+       </div>
+       
+       <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+       {slideImages.map((image, index) => (
+      <SwiperSlide key={index}>
+      <div>
+          <img src={image} alt={`slide_image_${index + 1}`} />
+          <button onClick={() => handleButtonClick(index + 1)}>
+              {/* SVG ikona spremanja */}
+              <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+              >
+                  <path
+                      d="M17 3H7a2 2 0 00-2 2v16l7-3 7 3V5a2 2 0 00-2-2z"
+                      stroke="#000" /* Crna boja obrisa */
+                      strokeWidth="1.5"
+                      fill="none"
+                  />
+              </svg>
+          </button>
+      </div>
+  </SwiperSlide>
+  
+))}
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+       </Swiper>      
+    </>
+    );
+};
+export default HomePage;
