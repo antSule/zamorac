50f93fde8c43bfd44df88bf1f68d166631f81dd2
aa53782 <aa53782@fer.hr>
2025-01-24 23:20:05 +0100

    testovi

---
+++ backend/src/test/java/hr/fer/progi/ticketmestar/AppUserTest.java
@@ -0,0 +1,36 @@
+package hr.fer.progi.ticketmestar;
+
+import hr.fer.progi.ticketmestar.service.impl.AppUserServiceJpa;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@TestPropertySource(properties = "spring.sql.init.mode=NEVER")
+@SpringBootTest
+public class AppUserTest {
+    private static final Logger logger = LoggerFactory.getLogger(AppUserTest.class);
+
+    @Autowired
+    private AppUserServiceJpa appUserServiceJpa;
+
+    @Test
+    void appUserIsNullTest() {
+        logger.info("Calling createAppUser method and expecting an IllegalArgumentException");
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            appUserServiceJpa.createAppUser(null);
+        });
+        logger.info("Expected exception thrown, checking if the message is 'AppUser object must be given'");
+        assertEquals("AppUser object must be given", exception.getMessage());
+        logger.info("Test successful.");
+    }
+}
