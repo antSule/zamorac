fb4109d0ac67d3320efe34ca17402a79144fb06f
Ana Angelov <aa53782@fer.hr>
2025-01-24 15:33:12 +0100

    Update ComponentTests.java

--- backend/src/test/java/hr/fer/progi/ticketmestar/ComponentTests.java
+++ backend/src/test/java/hr/fer/progi/ticketmestar/ComponentTests.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
